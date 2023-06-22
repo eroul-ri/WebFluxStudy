f0045ddf1991f9e2654c4f9f44f0e9c1d386622c
eroul-ri <bhr139313@gmail.com>
2023-06-22 14:13:00 +0900

    flux 연습

--- src/test/java/hr/flux/webfluxdemo/ReactorTest.java
+++ src/test/java/hr/flux/webfluxdemo/ReactorTest.java
@@ -1,12 +1,17 @@
 package hr.flux.webfluxdemo;
 
+import org.assertj.core.groups.Tuple;
 import org.junit.jupiter.api.Test;
+import org.springframework.util.StringUtils;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Hooks;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
+import reactor.util.function.Tuple2;
 
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class ReactorTest {
     @Test
@@ -22,14 +27,17 @@ public class ReactorTest {
         Flux<String> fruit = Flux.just("Apple", "Orange", "Grape");
 
         StepVerifier.create(fruit)
-                    .expectNext("Apple")
-                    .expectNext("Orange")
-                    .expectNext("Grape")
-                    .verifyComplete();
+                .expectNext("Apple")
+                .expectNext("Orange")
+                .expectNext("Grape")
+                .verifyComplete();
     }
 
     public String [] getFruitArray() {
         return new String[] {"Apple", "Orange", "Grape"};
+    }
+    public String [] getColorArray() {
+        return new String[] {"RED", "BLACK", "BLUE", "PINK"};
     }
     @Test
     public void createFluxFromArray() {
@@ -79,7 +87,7 @@ public class ReactorTest {
     @Test
     public void createFluxInterval() {
         Flux<Long> intervalFlux = Flux.interval(Duration.ofSeconds(1))
-                                                        .take(5);
+                                       .take(5);
 
         StepVerifier.create(intervalFlux)
                 .expectNext(0L)
@@ -87,6 +95,73 @@ public class ReactorTest {
                 .expectNext(2L)
                 .expectNext(3L)
                 .expectNext(4L)
+                .verifyComplete();
+    }
+
+    public Flux<String> arrayToFlux(String [] arr) {
+        return Flux.fromArray(arr);
+    }
+
+    @Test
+    public void mergeFluxes() {
+        String [] colors = getColorArray();
+        String [] fruit = getFruitArray();
+
+        Flux<String> colorFlux = arrayToFlux(colors);
+        Flux<String> fruitFlux = arrayToFlux(fruit);
+
+        Flux<String> mergedFlux = colorFlux.mergeWith(fruitFlux);
+
+        StepVerifier.create(mergedFlux)
+                .expectNext("RED")
+                .expectNext("BLACK")
+                .expectNext("BLUE")
+                .expectNext("PINK")
+                .expectNext("Apple")
+                .expectNext("Orange")
+                .expectNext("Grape")
+                .verifyComplete();
+    }
+
+    @Test
+    public void mergeFluxesDelay() {
+        String [] colors = getColorArray();
+        String [] fruit = getFruitArray();
+
+        Flux<String> colorFlux = arrayToFlux(colors)
+                                    .delayElements(Duration.ofMillis(500));
+        Flux<String> fruitFlux = arrayToFlux(fruit)
+                                    .delaySubscription(Duration.ofMillis(250))
+                                    .delayElements(Duration.ofMillis(500));
+
+        Flux<String> mergedFlux = colorFlux.mergeWith(fruitFlux);
+
+        // mergedFlux.subscribe(System.out::println); delay를 주게 되면, 다른 스레드에서 delayElements를 가짐. (Default Parallel Scheduler)
+        StepVerifier.create(mergedFlux)
+                .expectNext("RED")
+                .expectNext("Apple")
+                .expectNext("BLACK")
+                .expectNext("Orange")
+                .expectNext("BLUE")
+                .expectNext("Grape")
+                .expectNext("PINK")
+                .verifyComplete();
+    }
+
+    @Test
+    public void zipFluxes() {
+        String [] colors = getColorArray();
+        String [] fruit = getFruitArray();
+
+        Flux<String> colorFlux = arrayToFlux(colors);
+        Flux<String> fruitFlux = arrayToFlux(fruit);
+        // 한 항목씩 번갈아가져 옴.
+        Flux<Tuple2<String, String>> zippedFlux = Flux.zip(colorFlux, fruitFlux);
+
+        StepVerifier.create(zippedFlux)
+                .expectNextMatches((pre) -> pre.getT1().equals("RED") && pre.getT2().equals("Apple"))
+                .expectNextMatches((pre) -> pre.getT1().equals("BLACK") && pre.getT2().equals("Orange"))
+                .expectNextMatches((pre) -> pre.getT1().equals("BLUE") && pre.getT2().equals("Grape"))
                 .verifyComplete();
     }
 }
