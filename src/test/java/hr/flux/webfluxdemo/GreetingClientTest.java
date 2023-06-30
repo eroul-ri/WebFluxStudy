4d29a380022ead434f0e209a1e917fb68f541502
eroul-ri <bhr139313@gmail.com>
2023-06-30 16:35:17 +0900

    WebFluxTest anno

--- src/test/java/hr/flux/webfluxdemo/GreetingClientTest.java
+++ src/test/java/hr/flux/webfluxdemo/GreetingClientTest.java
@@ -2,11 +2,12 @@ package hr.flux.webfluxdemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WebFluxTest
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class GreetingClientTest {
 
     @Autowired
